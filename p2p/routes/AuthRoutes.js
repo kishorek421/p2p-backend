ca341e32660d6025cd11c63878befc94c9a4ce23
kishorek <kishorek@entoo.com>
2024-11-26 20:46:33 +0530

    space added

--- p2p/routes/AuthRoutes.js
+++ p2p/routes/AuthRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/AuthController');
 
+
 router.post('/refreshToken', authController.refreshToken);
 
 module.exports = router;
